ac9428ea3fb12ec5bed9e43cf9d59438044e6f16
Ankit Shokeen <ankitshokeen2001@gmail.com>
2023-06-03 23:52:09 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (51.67%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String str = s.trim();
+        int j = 0;
+        
+        for (int i=str.length() -1; i>=0; i--){
+            if (str.charAt(i) != ' '){
+                j++;
+            } else {
+                break;
+            }
+        }
+        return j;
+    }
+}
