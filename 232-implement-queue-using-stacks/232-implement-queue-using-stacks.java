a770c933454dbe4f00bcc41cd1adacc17571ce98
Ankit Shokeen <ankitshokeen2001@gmail.com>
2023-07-06 19:52:54 +0530

    Time: 0 ms (100.00%), Space: 40 MB (97.29%) - LeetHub

---
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
@@ -0,0 +1,43 @@
+class MyQueue {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+    
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stack1.push(x);
+    }
+    
+    public int pop() {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        int removed = stack2.pop();
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+        return removed;
+    }
+        
+    
+    
+    public int peek() {
+        while (!stack1.isEmpty()) {
+      stack2.push(stack1.pop());
+    }
+
+    int peeked = stack2.peek();
+
+    while (!stack2.isEmpty()) {
+      stack1.push(stack2.pop());
+    }
+    return peeked;
+    }
+    public boolean empty() {
+        return stack1.isEmpty();
+    }
+    
+}
