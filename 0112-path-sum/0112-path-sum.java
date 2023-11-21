eedccdbf37d8ffd7ebc7e73e270bc52c30c9c4c4
Ankit Shokeen <ankitshokeen2001@gmail.com>
2023-11-22 00:15:29 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (10.92%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,15 +14,17 @@
  * }
  */
 class Solution {
+    int sum = 0;
+    
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) {
             return false;
         }
         
-        if (root.val == targetSum && root.left == null && root.right == null) {
+        if (targetSum == root.val && root.left == null && root.right == null) {
             return true;
         }
         
-        return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right, targetSum-root.val);
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 }
