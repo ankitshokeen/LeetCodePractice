6b6c54980750affb385406130a8b21152a64a84c
Ankit Shokeen <ankitshokeen2001@gmail.com>
2024-02-04 23:29:23 +0530

    Time: 3 ms (89.05%), Space: 40.8 MB (90.00%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -5,7 +5,7 @@ class Solution {
         
         for (int i=0; i<coins.length; i++) {
             for (int j=coins[i]; j<dp.length; j++) {
-                dp[j] += dp[j -coins[i]];
+                dp[j] += dp[j - coins[i]];
             }
         }
         
