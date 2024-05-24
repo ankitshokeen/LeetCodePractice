e78cb119970aef8d6bdaf3c498e1faa4aa3b588b
Ankit Shokeen <ankitshokeen2001@gmail.com>
2024-05-25 02:05:30 +0530

    Time: 2 ms (51.33%), Space: 42.9 MB (65.62%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        Stack<String> ans = new Stack<>();
+        sol(n, "", 0, 0, ans);
+        
+        return ans;
+    }
+    
+    void sol(int n, String p, int open, int close, Stack<String> ans) {
+        if (p.length() == n*2) {
+            ans.push(p);
+        }
+        
+        if (open < n) {
+            sol(n, p + '(', open +1, close, ans);
+        }
+        if (close < open) {
+            sol(n, p + ')', open, close +1, ans);
+        }
+        
+        p = "";
+        open = 0;
+        close = 0;
+    }
+}
